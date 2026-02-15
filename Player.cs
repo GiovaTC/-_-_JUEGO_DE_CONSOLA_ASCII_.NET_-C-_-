8e47e3f449579d188e70436e6b83a5b26e43d655
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-15 12:09:03 -0500

    commit - Player .

--- Player.cs
+++ Player.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ascii_terminal_game
 {
-    internal class Player
+    public class Player
     {
-    }
-}
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }   
+}   
