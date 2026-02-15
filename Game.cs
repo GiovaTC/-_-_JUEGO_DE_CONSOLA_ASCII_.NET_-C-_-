76960a9cdbbe93c0c8998a59be4c0c0cbaec6030
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-15 12:13:17 -0500

    commit-Game .

--- Game.cs
+++ Game.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ascii_terminal_game
 {
-    internal class Game
+    public static class Game
     {
-        internal static void Start()
+        static int width = 40;
+        static int height = 20;
+        static Player player;
+        public static void Start()
+        {
+            player = new Player(width / 2, height / 2);
+
+            Console.Clear();
+            ConsoleKey key;
+
+            do
+            {
+                DrawGame();
+                key = Console.ReadKey(true).Key;
+                UpdatePlayer(key);
+
+            } while (key != ConsoleKey.Escape);
+
+            Menu.ShowMainMenu();
+        }
+        private static void DrawGame()
+        {
+            throw new NotImplementedException();
+        }
+        private static void UpdatePlayer(ConsoleKey key)
         {
             throw new NotImplementedException();
         }
     }
-}
+}   
