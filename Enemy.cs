c7c55b0f320f057d51343e11ee264ce70cf3c1bc
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-16 10:56:14 -0500

    commit - Enemy / .

---
+++ Enemy.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ascii_terminal_game
+{
+    internal class Enemy
+    {
+        public int X;
+        public int Y;
+
+        public Enemy(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}   
